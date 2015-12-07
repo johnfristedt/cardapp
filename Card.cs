aaaf2d471570dc0d6814c38c69b1812bf8cf858d
john <john@kodalagom.se>
2015-12-07 14:02:27 +0100

    HigherThan LowerThan Card Own file

---
+++ Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardApp
+{
+    class Card
+    {
+        public int Color { get; set; }
+        public int Value { get; set; }
+
+        public Card(int color, int value)
+        {
+            this.Color = color;
+            this.Value = value;
+        }
+
+        public static bool SameColor(Card card1, Card card2)
+        {
+            return card1.Color == card2.Color;
+        }
+
+        public static bool SameValue(Card card1, Card card2)
+        {
+            return card1.Value == card2.Value;
+        }
+
+        public bool LessThan(Card compare)
+        {
+            return this.Value < compare.Value;
+        }
+
+        public bool HigherThan(Card compare)
+        {
+            return this.Value > compare.Value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,28 +6,6 @@ using System.Threading.Tasks;
 
 namespace CardApp
 {
-    class Card
-    {
-        public int Color { get; set; }
-        public int Value { get; set; }
-
-        public Card(int color, int value)
-        {
-            this.Color = color;
-            this.Value = value;
-        }
-
-        public static bool SameColor(Card card1, Card card2)
-        {
-            return card1.Color == card2.Color;
-        }
-
-        public static bool SameValue(Card card1, Card card2)
-        {
-            return card1.Value == card2.Value;
-        }
-    }
-
     class Deck
     {
         public List<Card> Cards { get; set; }
